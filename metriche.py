addae2902d90315472d7f76909dc6d3d3f0968cd
RobertoGianfelice <rob.gianfelice@gmail.com>
2019-07-13 19:35:04 +0200

    rinominata funzione rime in metriche

--- metriche.py
+++ metriche.py
@@ -3,18 +3,18 @@ a=3
 b=1
 c=1
 
-def rime(a,b,c,metrica):
+def metriche(a,b,c,metrica):
   l=len(metrica)
   if len(metrica)==5:
     print("metrica=",metrica)
   else:
     if a>0:
-      rime(a-1,b,c,metrica+"a")
+      metriche(a-1,b,c,metrica+"a")
 
     if b>0 and l>=1:
-      rime(a,b-1,c,metrica+"b")
+      metriche(a,b-1,c,metrica+"b")
 
     if c>0 and l>=2:
-      rime(a,b,c-1,metrica+"c")
+      metriche(a,b,c-1,metrica+"c")
 
-rime(3,1,1,"")
+metriche(3,1,1,"")
