6f0498a4755683a3ec8bd2c1df3f786bc3b4ce11
RobertoGianfelice <rob.gianfelice@gmail.com>
2019-07-13 19:39:32 +0200

    Genera tutte le metriche n,3

--- metriche.py
+++ metriche.py
@@ -1,20 +1,18 @@
-# Genera le metriche 5,3 con il solo schema 3,1,1
-a=3
-b=1
-c=1
+# Genera tutte le metriche n,3
+def metriche(a,b,c,n,m,metrica):
 
-def metriche(a,b,c,metrica):
   l=len(metrica)
-  if len(metrica)==5:
+  completo=a>0 and b>0 and c>0
+  if l==n and completo:
     print("metrica=",metrica)
   else:
-    if a>0:
-      metriche(a-1,b,c,metrica+"a")
+    if a+b+c<=n and a<=n-m+1 and l>=0:
+      metriche(a+1,b,c,n,m,metrica+"a")
 
-    if b>0 and l>=1:
-      metriche(a,b-1,c,metrica+"b")
+    if a+b+c<=n and b<=n-m+1 and l>=1 and a>0:
+      metriche(a,b+1,c,n,m,metrica+"b")
 
-    if c>0 and l>=2:
-      metriche(a,b,c-1,metrica+"c")
+    if a+b+c<=n and c<=n-m+1 and l>=2 and b>0:
+      metriche(a,b,c+1,n,m,metrica+"c")
 
-metriche(3,1,1,"")
+metriche(0,0,0,3,3,"")
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 Programma per la generazione di metriche universali
 * generazione di tutte le possibili metriche di n versi in m rime
+
+* aggiornamento con Genera tutte le metriche n,3
